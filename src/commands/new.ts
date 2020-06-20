13058d2b8dec6fd680a0c7a521d31bb60db661ea
João Carvalho - Level 4 <joaocarvalhowd@outlook.com>
2020-06-20 19:47:44 -0300

    Add providers (#1)
    Co-authored-by: João Carvalho <oi@joaocarvalhowd.com.br>

--- src/commands/new.ts
+++ src/commands/new.ts
@@ -1,9 +1,21 @@
+import { prompt } from 'gluegun'
+
 module.exports = {
   name: 'new',
   alias: 'n',
   run: async function(toolbox) {
     const { print } = toolbox
 
+    const result = await prompt.ask([
+      {
+	type: 'select',
+	name: 'provider',
+	message: 'Which provider you want to use?',
+	choices: ['AWS', 'Google Cloud'],
+      }
+    ])
+
     print.success(`${print.checkmark} Created!`)
+    print.debug(result)
   }
 }
